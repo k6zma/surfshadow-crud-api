757d2dc9a440ed718081497dc75f87f1f0f9d58a
k6zma <87338187+k6zma@users.noreply.github.com>
2025-05-30 01:19:17 +0300

    feat(logger): add base implementation of logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,39 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+	"sync"
+)
+
+var once sync.Once
+
+func InitLogger(logLevel string) {
+	once.Do(func() {
+		var handler slog.Handler
+
+		level := strToSlogLevel(logLevel)
+		handler = newJSONLogHandler(os.Stdout, level)
+
+		logger := slog.New(handler)
+		slog.SetDefault(logger)
+	})
+}
+
+func strToSlogLevel(level string) slog.Level {
+	level = strings.ToLower(level)
+
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelDebug
+	}
+}
