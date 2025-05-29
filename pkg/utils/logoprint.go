fe3a7392265cde5a4602e6ace072bb7d86f01ffa
k6zma <87338187+k6zma@users.noreply.github.com>
2025-05-30 01:24:22 +0300

    feat(logo print): add implementation of pkg fucntion for beautiful print

--- pkg/utils/logoprint.go
+++ pkg/utils/logoprint.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	interval = 3 * time.Millisecond
+)
+
+func LogoPrint(text string) {
+	for _, v := range text {
+		fmt.Printf("%c", v)
+		time.Sleep(interval)
+	}
+}
